6d02826d061e1f7b9fd9f4ba861ad00f00118b4f
Ahda Filza Ghaffaru <99827242+Ahdaaa@users.noreply.github.com>
2023-03-20 15:25:04 +0700

    Update catatan-keuangan_entity.go

--- entity/catatan-keuangan_entity.go
+++ entity/catatan-keuangan_entity.go
@@ -1,11 +1,12 @@
 package entity
 
 type CatatanKeuangan struct {
-	ID           uint64  `json:"id" gorm:"primaryKey"`
-	JenisCatatan string  `json:"jenis_catatan" binding:"required"` // berupa pengeluaran, pemasukan, transfer atau apa
-	Nominal      uint64  `json:"nominal" binding:"required"`       // tuliskan nominal pengeluaran, pemasukannya
-	DompetID     uint64  `gorm:"foreignKey" json:"user_id"`
-	Dompet       *Dompet `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"user,omitempty"`
+	ID          uint64  `json:"id" gorm:"primaryKey"`
+	Deskripsi   string  `json:"deskripsi" binding:"required"`   // berupa detail dari pemasukan/pengeluaran
+	Pemasukan   uint64  `json:"pemasukan" binding:"required"`   // tuliskan nominal pemasukan
+	Pengeluaran uint64  `json:"pengeluaran" binding:"required"` // tuliskan nominal pengeluaran
+	DompetID    uint64  `gorm:"foreignKey" json:"user_id"`
+	Dompet      *Dompet `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"user,omitempty"`
 }
 
 func (CatatanKeuangan) TableName() string {
