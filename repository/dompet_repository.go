a7651ab8422f698dc9569c4a826859dbf8953155
Ahda Filza Ghaffaru <99827242+Ahdaaa@users.noreply.github.com>
2023-03-26 23:55:00 +0700

    Update dompet_repository.go

--- repository/dompet_repository.go
+++ repository/dompet_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"dompet-api/entity"
 
 	"gorm.io/gorm"
@@ -13,6 +14,8 @@ type dompetRepository struct {
 type DompetRepository interface {
 	// functional
 	GetMyDompet(tx *gorm.DB, id uint64) (entity.User, error)
+	GetDetailDompet(tx *gorm.DB, id uint64) (entity.Dompet, error)
+	InviteToDompet(tx *gorm.DB, idDompet uint64, emailUser string) (entity.User, error)
 }
 
 func NewDompetRepository(db *gorm.DB) DompetRepository {
@@ -37,3 +40,55 @@ func (r *dompetRepository) GetMyDompet(tx *gorm.DB, id uint64) (entity.User, err
 
 	return user, nil
 }
+
+func (r *dompetRepository) GetDetailDompet(tx *gorm.DB, id uint64) (entity.Dompet, error) {
+	var dompet entity.Dompet
+	var err error
+	if tx == nil {
+		tx = r.db.Where("id = ?", id).Preload("ListUser").Preload("ListCatatanKeuangan").Take(&dompet)
+		err = tx.Error
+	} else {
+		err = tx.Where("id = ?", id).Preload("ListUser").Preload("ListCatatanKeuangan").Take(&dompet).Error
+	}
+
+	if err != nil {
+		return entity.Dompet{}, err
+	}
+
+	return dompet, nil
+
+}
+
+func (r *dompetRepository) InviteToDompet(tx *gorm.DB, idDompet uint64, emailUser string) (entity.User, error) {
+	var dompet entity.Dompet
+	var newUser entity.User
+
+	checkDompet := r.db.Where("id = ?", idDompet).Take(&dompet) // cek dompet apakah ada
+	if checkDompet.Error != nil {
+		return entity.User{}, errors.New("dompet tidak tersedia")
+	}
+
+	checkUser := r.db.Where("email = ?", emailUser).Take(&newUser) // cek user dengan email tersebut apakah ada
+	if checkUser.Error != nil {
+		return entity.User{}, errors.New("email user tidak valid")
+	}
+
+	var detail []entity.DetailUserDompet
+	r.db.Where("dompet_id = ?", idDompet).Find(&detail) // ambil dompet yang ingin ditambah user baru
+
+	var UserID []uint64
+	for _, cek := range detail { // dari dompet yang sebelumnya diambil, extract id user siapa saja yang ada pada dompet.
+		UserID = append(UserID, cek.UserID) // ambil seluruh id user dan simpan pada array, untuk pengecekan duplikat
+	}
+
+	for _, cek := range UserID {
+		if cek == newUser.ID {
+			return entity.User{}, errors.New("user sudah ada pada dompet")
+		}
+	}
+
+	r.db.Model(&dompet).Association("ListUser").Append(&newUser)
+
+	return newUser, nil
+}
+
