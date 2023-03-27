262cc238e8ef30e14ee0f5cbec12bfae3fbffbaa
jap <javakanaya@gmail.com>
2023-03-27 17:44:41 +0700

    fix: cannot create dompet on saldo 0

--- dto/dompet_dto.go
+++ dto/dompet_dto.go
@@ -1,13 +1,12 @@
 package dto
 
 type DompetCreateDTO struct {
-	NamaDompet string `json:"nama_dompet" binding:"required"`
-	UserID     uint64 `json:"user_id"`
-	Saldo      uint64 `json:"saldo" binding:"required"`
+	NamaDompet string  `json:"nama_dompet" binding:"required"`
+	UserID     uint64  `json:"user_id"`
+	Saldo      *uint64 `json:"saldo" binding:"required"`
 }
 
 type InviteUserRequest struct {
 	DompetID  uint64 `json:"dompet_id"`
 	EmailUser string `json:"user_email" binding:"required"`
 }
-
