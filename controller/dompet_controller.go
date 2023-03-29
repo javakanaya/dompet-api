56ab3bf4fe28f6f9bcc528c27db76e6d59718ed6
Ahda Filza Ghaffaru <99827242+Ahdaaa@users.noreply.github.com>
2023-03-29 11:04:58 +0700

    Update dompet_controller.go

--- controller/dompet_controller.go
+++ controller/dompet_controller.go
@@ -78,7 +78,20 @@ func (c *dompetController) CreateDompet(ctx *gin.Context) {
 	res := utils.BuildResponse("Success to  create dompet", http.StatusOK, result)
 	ctx.JSON(http.StatusOK, res)
 }
+
 func (c *dompetController) DetailDompet(ctx *gin.Context) {
+
+	token := ctx.GetHeader("Authorization")
+	token = strings.Replace(token, "Bearer ", "", -1)
+	tokenService := service.NewJWTService()
+
+	idUser, err := tokenService.GetUserIDByToken(token)
+	if err != nil {
+		response := utils.BuildErrorResponse("gagal memproses request", http.StatusBadRequest)
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
 	idDompet, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
 	if err != nil {
 		response := utils.BuildErrorResponse("gagal memproses request", http.StatusBadRequest)
@@ -86,9 +99,9 @@ func (c *dompetController) DetailDompet(ctx *gin.Context) {
 		return
 	}
 
-	result, err := c.dompetService.GetDetailDompet(idDompet)
+	result, err := c.dompetService.GetDetailDompet(idDompet, idUser)
 	if err != nil {
-		response := utils.BuildErrorResponse("invalid ID", http.StatusBadRequest)
+		response := utils.BuildErrorResponse(err.Error(), http.StatusBadRequest)
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
 		return
 	}
