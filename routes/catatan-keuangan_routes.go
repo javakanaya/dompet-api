41ed54dc699b635b21c56e8cead3a61d510cc35e
Ahda Filza Ghaffaru <99827242+Ahdaaa@users.noreply.github.com>
2023-03-31 18:10:21 +0700

    Create catatan-keuangan_routes.go

---
+++ routes/catatan-keuangan_routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"dompet-api/controller"
+	"dompet-api/middleware"
+	"dompet-api/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CatatanRouter(router *gin.Engine, catatanController controller.CatatanController, jwtService service.JWTService) {
+	catatanRoutes := router.Group("/secured").Use(middleware.Authenticate())
+	{
+		catatanRoutes.POST("/transfer/:id", catatanController.Transfer)
+	}
+
+	adminRoutes := router.Group("/admin")
+	{
+		adminRoutes.POST("/kategori", catatanController.InsertKategori)
+	}
+}
